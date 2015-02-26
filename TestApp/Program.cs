8d054243f379c5570aa472875905c89644e12841
rumanmainul <ruman.mainul@yahoo.com>
2015-02-26 09:12:23 +0600

    test project
    it was my 1t project

---
+++ TestApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Student aStudent = new Student();
+
+            aStudent.GetInformation();
+
+        }
+    }
+}
---
+++ TestApp/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Student
+    {
+        public string Name { set; get; }
+        public string RegNo { set; get; }
+        public string Email { set; get; }
+
+        public string GetInformation()
+        {
+            return Name;
+        }
+    }
+}
